14b7bc08a7962970e5bd5e2c386d112ea8303f7b
Ascura-Austin <austin.ascura@gmail.com>
2020-03-11 00:57:03 -0400

    adding coordinate class

---
+++ MarsRoverProblem/MarsRoverProblem/Coordinate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverProblem
+{
+    class Coordinate
+    {
+        public readonly int XMAX;
+        public readonly int YMAX;
+
+        private int _PosX;
+        private int _PosY;
+
+        public int PosX
+        {
+            get
+            {
+                return PosX;
+            }
+
+            set
+            {
+                _PosX += value;
+                if (_PosX > XMAX)
+                    _PosX -= XMAX;
+            }
+        }
+
+        public int PosY
+        {
+            get
+            {
+                return PosY;
+            }
+
+            set
+            {
+                _PosY += value;
+                if (_PosY > YMAX)
+                    _PosY -= YMAX;
+            }
+        }
+
+        public Coordinate(int xMax, int yMax)
+        {
+            XMAX = xMax;
+            YMAX = yMax;
+        }
+    }
+}
